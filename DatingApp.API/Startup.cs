b4979e77a3559513840df52dcf49265a1cbf8cd9
Juan Pablo Perez <Juan.Pablo.Perez@retailinmotion.com>
2018-08-04 10:20:14 +0100

    Extending the API

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Buffers;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp.API.Data;
 using DatingApp.API.Helpers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -13,12 +15,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
 
 namespace DatingApp.API
 {
@@ -35,9 +39,15 @@ namespace DatingApp.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddJsonOptions(opt => {
+                    opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
             services.AddCors();
+            services.AddAutoMapper();
+            services.AddTransient<Seed>();
             services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IDatingRepository, DatingRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options => {
                     options.TokenValidationParameters = new TokenValidationParameters
@@ -49,10 +59,20 @@ namespace DatingApp.API
                         ValidateAudience = false
                     };
                 });
+            // To avoid Self referencing loop detected for property 'user' with type 'DatingApp.API.Models.User'
+            // https://stackoverflow.com/questions/50879970/entity-framework-include-is-including-children-of-children
+            // services.AddMvc(options =>
+            // {
+            //     options.OutputFormatters.Clear();
+            //     options.OutputFormatters.Add(new JsonOutputFormatter(new JsonSerializerSettings()
+            //     {
+            //         ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            //     }, ArrayPool<char>.Shared));
+            // });                
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Seed seeder)
         {
             if (env.IsDevelopment())
             {
@@ -74,6 +94,8 @@ namespace DatingApp.API
             }
 
             // app.UseHttpsRedirection();
+            // We just need this to be executed once
+            // seeder.SeedUsers();
             app.UseCors( x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseMvc();
